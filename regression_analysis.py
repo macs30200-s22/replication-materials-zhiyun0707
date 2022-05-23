698c143d9e28dd36f3b6f35620d98418b1b917de
zhiyun0707 <89923088+zhiyun0707@users.noreply.github.com>
2022-05-22 23:38:23 -0500

    Update regression_analysis.py

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,7 +1,6 @@
 import statsmodels.api as sm
 regression_analysis = pd.read_csv("regression_analysis.csv")
-X = regression_analysis[['cases', 'poverty', 'education', 'gender', 'white', 'black', 'ethnicity', 
-                         'laborforce']]
+X = regression_analysis[['cases', 'poverty', 'education', 'gender', 'non-white', 'ethnicity']]
 y = regression_analysis['income']
 data = sm.add_constant(X)
 model = sm.OLS(y, data).fit()
